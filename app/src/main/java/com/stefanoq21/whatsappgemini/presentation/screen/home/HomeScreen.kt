3026ffc7c5715e00d4be7aa959b14651eb378e51
DESKTOP-P3D71NI\Stefano <stefanoq21@hotmail.it>
2025-11-23 12:13:26 +0100

    Clean up MainActivity and add screenshot testing support
    - Remove unused variables and commented-out code in MainActivity
    - Update Kotlin Android plugin version to 2.2.0
    - Add screenshot-validation-api dependency to version catalog
    - Add HomeScreenTest for screenshot testing
    - Include screenshot validation dependencies in app/build.gradle.kts

--- app/src/main/java/com/stefanoq21/whatsappgemini/presentation/screen/home/HomeScreen.kt
+++ app/src/main/java/com/stefanoq21/whatsappgemini/presentation/screen/home/HomeScreen.kt
@@ -35,6 +35,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.stefanoq21.whatsappgemini.presentation.component.home.HomeTopBar
 import com.stefanoq21.whatsappgemini.presentation.navigation.BottomBarEnum
